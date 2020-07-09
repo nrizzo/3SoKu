0db6a59e4225dfc9ed5028ef71942745af0029f8
nrizzo <nrizzo@nrizzothink.localdomain>
2020-07-09 10:10:35 +0200

    ✅ Updated and fixed comparative tests

--- tests/JavaScript/solver.js
+++ tests/JavaScript/solver.js
@@ -3,13 +3,13 @@
  * 	http://www.nearly42.org/games/icosoku-solver/
  * (I have not asked permission to use this code). Function icosolve must be
  * parametrized to accept in input the description of the capacities using his
- * naming convention, and its output must be suppressed.
+ * naming convention.
  * ~ Nicola Rizzo
  */
 
 
 // Measure the solving time
 var start = new Date();
-icosolve([8,11,7,9,10,5,2,4,1,6,3,12,]);
+icosolve([x,x,x,x,x,x,x,x,x,x,x,x,]);
 var end = new Date();
 console.log((end - start)/1000 + "");
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,66 +3,49 @@
 # on instances of IcoSoKu.
 # ~ Nicola Rizzo
 
-# seed
-RANDOM="snubdisphenoid"
+RANDOM="snubdisphenoid" # seed
 
 echo -n "Generating batch of tests... "
 echo -n > batch
 for i in {1..100}
 do
-    # shuf is used to generate pseudo-random IcoSoKu instances
-    TEMP="$RANDOM"
-    shuf -i1-12 \
-	    --random-source=<(openssl enc -aes-256-ctr \
-	    -pass pass:"$TEMP" -nosalt </dev/zero 2>/dev/null) | \
-    tr "\n" " " >> batch
-    echo >> batch
+	# shuf is used to generate pseudo-random IcoSoKu instances
+	TEMP="$RANDOM"
+	shuf -i1-12 \
+		--random-source=<(openssl enc -aes-256-ctr \
+		-pass pass:"$TEMP" -nosalt </dev/zero 2>/dev/null) | \
+		tr "\n" " " >> batch
+	echo >> batch
 done
 echo "done."
 
 # MiniZinc tests
-echo -n "Testing MiniZinc model (gecode)... "
+echo -n "Testing MiniZinc model - Gecode... "
 echo -n > times_minizinc_gecode
-cd MiniZinc
+cd MiniZinc_gecode
 while read line
 do
-    input="cap = [$(echo $line | tr " " ",")];"
-    sed -i "1s/.*/$input/" input-ico.dzn
-
-    minizinc -v --solver gecode IcoSoKu.mzn input-ico.dzn 2>&1 | \
-    grep Done | \
-    cut -d' ' -f7 >> ../times_minizinc_gecode
+	./icosolve.sh $line >> ../times_minizinc_gecode
 done < ../batch
 cd ..
 echo "done."
 
-echo -n "Testing MiniZinc model (gecode - randomized + constant restart)... "
+echo -n "Testing MiniZinc model - Gecode, randomized search + constant restart... "
 echo -n > times_minizinc_gecode_random_restart
-cd MiniZinc
+cd MiniZinc_gecode_randomized
 while read line
 do
-    input="cap = [$(echo $line | tr " " ",")];"
-    sed -i "1s/.*/$input/" input-ico.dzn
-
-    minizinc -v -r snubdisphenoid --solver gecode \
-        IcoSoKu-random.mzn input-ico.dzn 2>&1 | \
-    grep Done | \
-    cut -d' ' -f7 >> ../times_minizinc_gecode_random_restart
+	./icosolve.sh $line >> ../times_minizinc_gecode_random_restart
 done < ../batch
 cd ..
 echo "done."
 
-echo -n "Testing MiniZinc model (chuffed)... "
+echo -n "Testing MiniZinc model - Chuffed... "
 echo -n > times_minizinc_chuffed
-cd Minizinc
+cd MiniZinc_chuffed
 while read line
 do
-    input="cap = [$(echo $line | tr " " ",")];"
-    sed -i "1s/.*/$input/" input-ico.dzn
-
-    minizinc -v --solver chuffed IcoSoKu.mzn input-ico.dzn 2>&1 | \
-    grep Done | \
-    cut -d' ' -f7 >> ../times_minizinc_chuffed
+    ./icosolve.sh $line >> ../times_minizinc_chuffed
 done < ../batch
 cd ..
 echo "done."
@@ -73,9 +56,7 @@ echo -n > times_asp
 cd ASP
 while read line
 do
-    ./random-ico-instance.sh $line
-    ./icosolve.sh 2>&1 | \
-      grep "CPU Time" | cut -d ' ' -f8 | tr -d 's' >> ../times_asp
+	./icosolve.sh $line >> ../times_asp
 done < ../batch
 cd ..
 echo "done."
@@ -86,11 +67,7 @@ echo -n > times_js
 cd JavaScript
 while read line
 do
-    # the naming of the vertices of the icosahedron is different!
-    array=($line)
-    input="${array[0]} ${array[2]} ${array[3]} ${array[4]} ${array[5]} ${array[1]} ${array[10]} ${array[9]} ${array[8]} ${array[7]} ${array[6]} ${array[11]}"
-    ./random-ico-instance.sh $input
-    ./solver.sh >> ../times_js
+    ./icosolve.sh $line >> ../times_js
 done < ../batch
 cd ..
 echo "done."
