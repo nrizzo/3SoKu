adcc6ec466509fdcc936ac3886aec8dfbaa4d141
nrizzo <nrizzo@nrizzothink.localdomain>
2020-07-10 10:15:37 +0200

    🔥 Removed JS solver in /solvers, it's in /tests; removed good_instances.c

--- solvers/JavaScript/random-ico-instance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script that generates a random instance of IcoSoKu by modifying line
-# containing "icosolve[...];" of file solver.js
-# ~ Nicola Rizzo
-
-input=$(shuf -i 1-12 | tr "\n" ",")
-
-sed -i "s/icosolve(\[.*/icosolve([$input]);/" solver.js
