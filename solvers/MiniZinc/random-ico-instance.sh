e8e1065df2e30c13372fe8078462f82b7785a66f
nrizzo <nrizzo@users.noreply.github.com>
2020-10-14 23:26:56 +0200

    🚚 The repository moved to nrizzo/3coSoKu

--- solvers/MiniZinc/random-ico-instance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script that generates a random instance of IcoSoKu, modifying the first
-# line of input-ico.dzn
-# ~ Nicola Rizzo
-
-foo="cap = [$(shuf -i 1-12 | tr "\n" "," | cut -d',' -f1-12)];"
-bar="$(tail -n +2 input-ico.dzn)"
-echo -e "$foo\n$bar" > input-ico.dzn
